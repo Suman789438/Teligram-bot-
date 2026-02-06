c499641cabbb8783028cd4fc008b93b729e5dfe2
THE SILENT HACKER RAJ <70suman28@gmail.com>
2026-02-06 21:01:16 +0530

    Remove bot token from configuration
    Removed the bot token for security reasons.

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from telebot.types import ChatPermissions
 # =====================
 # 🔧 CONFIG
 # =====================
-BOT_TOKEN = "8372879804:AAEKowoa_EaSy6TeA1aoT9jUNEm1pEeLXe8"
+BOT_TOKEN = ""
 
 FORBIDDEN_WORDS = [
     "dm me",
